956fc8c40c3652f37082bb1690a88f8d46b1b3b4
Bruna Amoretti Guimarães <brubru.amoretti@gmail.com>
2021-08-27 10:57:36 -0300

    Criação classe model Tema

--- Oceani/src/main/java/br/org/generation/oceani/OceaniApplication.java
+++ Oceani/src/main/java/br/org/generation/oceani/OceaniApplication.java
@@ -1,4 +1,4 @@
-package br.org.generation.hello;
+package br.org.generation.oceani;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Oceani/src/main/java/br/org/generation/oceani/model/TemaModel.java
@@ -0,0 +1,66 @@
+package br.org.generation.oceani.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_temas")
+public class TemaModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Size(min = 5, max = 50, message = "o atributo categoria deve ter no mínimo 5 e no máximo 50 caracteres")
+	@NotNull
+	private String categoria;
+	
+	@Size(min = 5, max = 500, message = "o atributo descricao deve ter no mínimo 5 e no máximo 500 caracteres")
+	@NotNull
+	private String descricao;
+	
+	@Size(min = 5, max = 250, message = "o atributo keywords deve ter no mínimo 5 e no máximo 250 caracteres")
+	@NotNull
+	private String keywords;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	
+
+
+}
