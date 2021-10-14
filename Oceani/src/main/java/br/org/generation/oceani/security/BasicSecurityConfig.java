0559a3d7d65fee9059f869cbcddd781571e3df7a
GabrielAlvao <albuquerquealves2002@hotmail.com>
2021-10-14 09:55:31 -0300

    liberando acesso do curtir

--- Oceani/src/main/java/br/org/generation/oceani/security/BasicSecurityConfig.java
+++ Oceani/src/main/java/br/org/generation/oceani/security/BasicSecurityConfig.java
@@ -38,7 +38,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
-			.antMatchers(HttpMethod.OPTIONS).permitAll()
+			.antMatchers("/postagem/curtir/{id}").permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
